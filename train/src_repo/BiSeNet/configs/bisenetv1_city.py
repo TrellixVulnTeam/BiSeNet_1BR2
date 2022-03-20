8a95b8a31445d1b0e0989f3952d8f3dab219dbf5
uyolo <101853326+uyolo1314@users.noreply.github.com>
2022-03-20 14:15:15 +0800

    faster training

--- train/src_repo/BiSeNet/configs/bisenetv1_city.py
+++ train/src_repo/BiSeNet/configs/bisenetv1_city.py
@@ -8,6 +8,7 @@ cfg = dict(
     warmup_iters=1000,
     max_iter=80000,
     dataset='CityScapes',
+    cache=True,
     im_root='./datasets/cityscapes',
     train_im_anns='./datasets/cityscapes/train.txt',
     val_im_anns='./datasets/cityscapes/val.txt',
